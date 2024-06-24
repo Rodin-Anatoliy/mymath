7d6eb6fb9e98b4a490a0867da6ac3792e3863862
Anatoly Rodin <rodin-tolya@mail.ru>
2024-06-25 01:19:54 +0700

    feat: test
    BREAKING CHANGE

--- mymath.go
+++ mymath.go
@@ -1,33 +1,39 @@
 package mymath
 
-import (
-	"math"
-)
+import "math"
 
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
+
 func Ceil(x float64) float64 {
 	return math.Ceil(x)
 }
+
 func Floor(x float64) float64 {
 	return math.Floor(x)
 }
+
 func Pow(x, y float64) float64 {
 	return math.Pow(x, y)
 }
+
 func Max(x, y float64) float64 {
 	return math.Max(x, y)
 }
+
 func Min(x, y float64) float64 {
 	return math.Min(x, y)
 }
+
 func Abs(x float64) float64 {
 	return math.Abs(x)
 }
+
 func Yn(x int, y float64) float64 {
 	return math.Yn(x, y)
 }
+
 func Acos(x float64) float64 {
 	return math.Acos(x)
 }
@@ -37,6 +43,7 @@ func Acosh(x float64) float64 {
 func Asin(x float64) float64 {
 	return math.Asin(x)
 }
+
 func Asinh(x float64) float64 {
 	return math.Asinh(x)
 }
